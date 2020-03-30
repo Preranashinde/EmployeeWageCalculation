742236e122df595b444fcc595df20cd756fc6b27
Preranashinde <preranapshinde3009@gmail.com>
2020-03-30 11:00:58 -0300

    By using switch case calculate employee wages

--- employeeWage.java
+++ employeeWage.java
@@ -1,29 +1,29 @@
 public class employeeWage
 {
-	public static void main ( String args[] )
+	public static void main ( String[] args )
 	{
-		//Variables
+     	int checkNumber=(int)(Math.floor(Math.random()*10)%3);
+    	//Variables
 		int salary=0, empHours=0;
 		//Constants
-		int IS_FULLTIME=1, IS_PARTTIME=2, WAGE_PER_HOUR=20;
-		int checkNumber=(int)(Math.floor(Math.random()*10)%3);
-		if ( checkNumber == IS_FULLTIME )
-		{
-			System.out.println("Employee Is Full Time");
-			empHours=8;
-		}
-		else
-		if ( checkNumber == IS_PARTTIME )
-      {
-         System.out.println("Employee Is Part Time");
-         empHours=4;
-      }
-		else
-		{
-			System.out.println("Employee Is Absent");
-			empHours=0;
-		}
-		salary=empHours*WAGE_PER_HOUR;
-		System.out.println("Salary of Employee is" +salary);
+    	final int IS_FULLTIME=1, IS_PARTTIME=2, WAGE_PER_HOUR=20;
+
+    	switch (checkNumber)
+   	{
+			case IS_FULLTIME:
+				System.out.println("Employee Is Full Time");
+   	      empHours=8;
+   	      break;
+   	   case IS_PARTTIME:
+  	        	System.out.println("Employee Is Part Time");
+ 	        	empHours=4;
+ 	        	break;
+ 	   	default:
+ 	        	System.out.println("Employee Is Absent");
+ 	        	empHours=0;
+ 	        	break;
+ 	 	}
+      	salary=empHours*WAGE_PER_HOUR;
+      	System.out.println("Salary of Employee is "+salary);
 	}
 }
