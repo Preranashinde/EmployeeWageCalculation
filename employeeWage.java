2d86b2af9f58969ce052d43dc4fdfdbeadf7babe
Preranashinde <preranapshinde3009@gmail.com>
2020-03-31 11:37:45 -0300

    To save the total wage for each company

--- employeeWage.java
+++ employeeWage.java
@@ -1,42 +1,56 @@
-public class employeeWage
-{	public static int salary=0;
-	public static int employeeWorkHours=0;
-   public static int totalSalary=0;
-   public static int totalEmployeeHours=0;
-   public static int noOfDays=0;
-	final int IS_FULL_TIME=1, IS_PART_TIME=2;
+public class employeeWageBuilder
+{	
+	public static final int IS_FULL_TIME=1, IS_PART_TIME=2;
+	public static int NUMBER_OF_WORKING_DAYS, WAGE_PER_HOUR, MAXIMUM_HOURS_IN_MONTH;
 
+//Variables
+	public static int totalWage=0;
+
+	public employeeWageBuilder( int numberOfWorkingDays, int wagePerHour, int maximumHoursInMonth )
+	{
+		this.NUMBER_OF_WORKING_DAYS = numberOfWorkingDays;
+		this.WAGE_PER_HOUR = wagePerHour;
+		this.MAXIMUM_HOURS_IN_MONTH = maximumHoursInMonth;
+	}
 	public static void main ( String[] args )
 	{
-		employeeWage employee= new employeeWage();
-		employee.wageCalculation("Microsoft India",20,25,110);
-		employee.wageCalculation("Mercedes Benz",22,30,100);
-
+		System.out.println("Employee details of Microsoft India is:");
+		employeeWageBuilder companyOne = new employeeWageBuilder(20,25,110);
+		System.out.println("Salary of employee of company one is");
+		calculationOfWage(companyOne);
+		System.out.println("Employee details of Mercedes Benz is:");
+		employeeWageBuilder companyTwo = new employeeWageBuilder(22,30,100);
+		System.out.println("Salary of employee of company two is");
+		calculationOfWage(companyTwo);
 	}
 
-	public void wageCalculation( String companyName, int MAXIMUM_DAYS, int WAGE_PER_HOUR, int MAXIMUM_HOURS )
+	private void calculateTotalWage(int totalWage){
+	this.totalWage =totalWage;
+	}
+	public static void calculationOfWage( employeeWageBuilder company )
 	{
-      while ( totalEmployeeHours < MAXIMUM_HOURS && noOfDays < MAXIMUM_DAYS )
+		int salary=0, totalEmployeeHours=0, noOfDays=0, totalWage=0, employeeWorkHours=0 ;
+      while ( totalEmployeeHours <=company.MAXIMUM_HOURS_IN_MONTH && noOfDays < company.NUMBER_OF_WORKING_DAYS )
       {
         int employeeCheck=(int)(Math.floor(Math.random()*10)%3);
         noOfDays+=1;
         switch (employeeCheck)
         {
 				case IS_FULL_TIME:
-            employeeWorkHours=8;
+             employeeWorkHours=8;
             break;
 				case IS_PART_TIME:
-            employeeWorkHours=4;
+             employeeWorkHours=4;
             break;
 				default:
-            employeeWorkHours=0;
+             employeeWorkHours=0;
             break;
         }
         salary=employeeWorkHours*WAGE_PER_HOUR;
         totalEmployeeHours+=employeeWorkHours;
-        totalSalary+=salary;
-        System.out.println("Salary of employee on "+noOfDays+ " is "+salary);
+        totalWage+=salary;
       }
-		System.out.println("Company name is: "+companyName+" Total salary of employee is:"+ totalSalary);
+		company.calculateTotalWage(totalWage);
+		System.out.println("Total salary of employee is"+totalWage);
 	}
 }
